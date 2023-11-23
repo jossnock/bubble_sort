cc184cbdaa19252c6624c23dc69b7a2f66497efe
jossnock <19121867@hlnsc.ac.uk>
2023-11-23 11:34:19 +0000

    stuff

--- Sorting with Bubbles.py
+++ Sorting with Bubbles.py
@@ -3,24 +3,24 @@ def bubble_sort(list,high_to_low = False):
     \n Add 'True' parameter after list to sort from largest to smallest."""
     sorted = False
     number_of_passes = 1
-    if high_to_low == True:
+    if high_to_low == False:
+            while sorted == False:
+                swapped = False
+                for i in range(len(list) - number_of_passes): #repeats for the number of swaps needed to complete a pass
+                    if list[i] > list[i + 1]: #if i > i+1, swap them
+                        temp = list[i]
+                        list[i] = list[i + 1]
+                        list[i + 1] = temp
+                        swapped = True
+                if swapped == False: #tests nothing has never been swapped for the pass, 
+                    sorted = True
+                number_of_passes += 1 #keeps track of number of passes to increase efficiency for the for loop
+            return list
+    else: #if you want it sorted high to low, this branch triggers
         while sorted == False:
             swapped = False
             for i in range(len(list) - number_of_passes): #repeats for the number of swaps needed to complete a pass
-                if list[i] < list[i + 1]: #if i > i+1, swap them
-                    temp = list[i]
-                    list[i] = list[i + 1]
-                    list[i + 1] = temp
-                    swapped = True
-            if swapped == False: #tests nothing has never been swapped for the pass, 
-                sorted = True
-            number_of_passes += 1 #keeps track of number of passes to increase efficiency for the for loop
-        return list
-    else:
-        while sorted == False:
-            swapped = False
-            for i in range(len(list) - number_of_passes): #repeats for the number of swaps needed to complete a pass
-                if list[i] > list[i + 1]: #if i > i+1, swap them
+                if list[i] < list[i + 1]: #if i < i+1, swap them
                     temp = list[i]
                     list[i] = list[i + 1]
                     list[i + 1] = temp
@@ -32,6 +32,6 @@ def bubble_sort(list,high_to_low = False):
 
 import math
 
-items = [6,5,math.inf,3,213,23,5667,math.factorial(8),23,-math.inf,676,43,34234,math.sin(23),32497823,54.04,math.inf,34.897,abs(-343),-8342.324,-23.22111211212]
+items = [6,5,math.inf,3,213,23,5667,math.factorial(8),23,-math.inf,343,34234,math.sin(23),324978,54.04,math.inf,34.897,abs(-343),-8342.324,-23.22111211212]
 
-print(bubble_sort(items,True))
+print(bubble_sort(items))
