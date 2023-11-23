72cbb5a77b49a1304791d3277786baf3f462bdac
jossnock <19121867@hlnsc.ac.uk>
2023-11-23 10:52:54 +0000

    clarification

--- Sorting with Bubbles.py
+++ Sorting with Bubbles.py
@@ -1,5 +1,6 @@
 def bubble_sort(list,high_to_low = False):
-    """Sorts a list of integers from smallest to largest, add parameter '-1' to sort from largest to smallest"""
+    """Sorts a list of integers from smallest to largest.
+    \n Add 'True' parameter after list to sort from largest to smallest."""
     sorted = False
     number_of_passes = 1
     if high_to_low == True:
@@ -33,4 +34,4 @@ import math
 
 items = [6,5,math.inf,3,213,23,5667,math.factorial(8),23,-math.inf,676,43,34234,math.sin(23),32497823,54.04,math.inf,34.897,abs(-343),-8342.324,-23.22111211212]
 
-print(bubble_sort(items))
+print(bubble_sort(items,True))
