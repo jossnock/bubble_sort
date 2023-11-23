be2e84d40c819952094a0889660b301550154ba1
jossnock <19121867@hlnsc.ac.uk>
2023-11-23 10:50:11 +0000

    added high to low order

--- Sorting with Bubbles.py
+++ Sorting with Bubbles.py
@@ -1,21 +1,36 @@
-def bubble_sort(list):
+def bubble_sort(list,high_to_low = False):
+    """Sorts a list of integers from smallest to largest, add parameter '-1' to sort from largest to smallest"""
     sorted = False
     number_of_passes = 1
-    while sorted == False:
-        swapped = False
-        for i in range(len(list) - number_of_passes):
-            if list[i] > list[i + 1]:
-                temp = list[i]
-                list[i] = list[i + 1]
-                list[i + 1] = temp
-                swapped = True
-        if swapped == False:
-            sorted = True
-        number_of_passes += 1
-    return list
+    if high_to_low == True:
+        while sorted == False:
+            swapped = False
+            for i in range(len(list) - number_of_passes): #repeats for the number of swaps needed to complete a pass
+                if list[i] < list[i + 1]: #if i > i+1, swap them
+                    temp = list[i]
+                    list[i] = list[i + 1]
+                    list[i + 1] = temp
+                    swapped = True
+            if swapped == False: #tests nothing has never been swapped for the pass, 
+                sorted = True
+            number_of_passes += 1 #keeps track of number of passes to increase efficiency for the for loop
+        return list
+    else:
+        while sorted == False:
+            swapped = False
+            for i in range(len(list) - number_of_passes): #repeats for the number of swaps needed to complete a pass
+                if list[i] > list[i + 1]: #if i > i+1, swap them
+                    temp = list[i]
+                    list[i] = list[i + 1]
+                    list[i + 1] = temp
+                    swapped = True
+            if swapped == False: #tests nothing has never been swapped for the pass, 
+                sorted = True
+            number_of_passes += 1 #keeps track of number of passes to increase efficiency for the for loop
+        return list
 
 import math
 
-items = [6,5,math.inf,3,213,23,5667,3332222344,23,-math.inf,676,43,34234,54.04,math.inf,34.897,-343,-8342.324]
+items = [6,5,math.inf,3,213,23,5667,math.factorial(8),23,-math.inf,676,43,34234,math.sin(23),32497823,54.04,math.inf,34.897,abs(-343),-8342.324,-23.22111211212]
 
 print(bubble_sort(items))
