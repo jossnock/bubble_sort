13c1a0d9983891c1d4cb36d95cdc4b0f526e8888
jossnock <19121867@hlnsc.ac.uk>
2023-12-08 15:06:03 +0000

    chopped

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -1,34 +1,20 @@
-def bubble_sort(list,high_to_low = False):
+def bubble_sort(list):
     """Sorts a list of integers from smallest to largest.
     \n Add 'True' parameter after list to sort from largest to smallest."""
     sorted = False
     number_of_passes = 1
-    if high_to_low == False:
-            while sorted == False:
-                swapped = False
-                for i in range(len(list) - number_of_passes): #repeats for the number of swaps needed to complete a pass
-                    if list[i] > list[i + 1]: #if i > i+1, swap them
-                        temp = list[i]
-                        list[i] = list[i + 1]
-                        list[i + 1] = temp
-                        swapped = True
-                if swapped == False: #tests nothing has never been swapped for the pass, 
-                    sorted = True
-                number_of_passes += 1 #keeps track of number of passes to increase efficiency for the for loop
-            return list
-    else: #if you want it sorted high to low, this branch triggers
-        while sorted == False:
-            swapped = False
-            for i in range(len(list) - number_of_passes): #repeats for the number of swaps needed to complete a pass
-                if list[i] < list[i + 1]: #if i < i+1, swap them
-                    temp = list[i]
-                    list[i] = list[i + 1]
-                    list[i + 1] = temp
-                    swapped = True
-            if swapped == False: #tests nothing has never been swapped for the pass, 
-                sorted = True
-            number_of_passes += 1 #keeps track of number of passes to increase efficiency for the for loop
-        return list
+    while sorted == False:
+        swapped = False
+        for i in range(len(list) - number_of_passes): #repeats for the number of swaps needed to complete a pass
+            if list[i] > list[i + 1]: #if i > i+1, swap them
+                temp = list[i]
+                list[i] = list[i + 1]
+                list[i + 1] = temp
+                swapped = True
+        if swapped == False: #tests nothing has never been swapped for the pass, 
+            sorted = True
+        number_of_passes += 1 #keeps track of number of passes to increase efficiency for the for loop
+    return list
 
 import math
 
